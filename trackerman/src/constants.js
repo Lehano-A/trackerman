b556e0300f1cf5d57b2f34125961beeb9111e38e
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-07-23 17:25:16 +0300

    Добавляет файл constants

---
+++ trackerman/src/constants.js
@@ -0,0 +1,14 @@
+const dataCards = [
+  {
+    name: 'Карабин альпинистский Аллигатор, длина: 80 мм, 2 шт',
+    dateAddition: '02.08.2024',
+    price: {
+      start: '247',
+      current: '230',
+      target: '195'
+    },
+    image: 'https://ir-3.ozone.ru/s3/multimedia-1-k/wc1000/7057025228.jpg'
+  }
+]
+
+export { dataCards }

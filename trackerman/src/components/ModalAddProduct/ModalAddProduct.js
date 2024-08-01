1411421aa83a8c406fc51f3b9ac474972e8bb76e
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-08-01 17:52:40 +0300

    Добавляет кнопку закрытия модального окна добавления товара, а также заменяет иконку close

--- trackerman/src/components/ModalAddProduct/ModalAddProduct.js
+++ trackerman/src/components/ModalAddProduct/ModalAddProduct.js
@@ -1,6 +1,8 @@
 import styled from "styled-components"
+import close from "../../images/icons/close.svg"
 
 const Modal = styled.dialog`
+  position: relative;
   max-width: 650px;
   width: 100%;
   z-index: 99999;
@@ -14,6 +16,30 @@ const Modal = styled.dialog`
 
   &:focus {
     border: none;
+  }
+`
+
+const ButtonClose = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+  border: 0;
+  background-color: transparent;
+`
+
+const IconClose = styled.img`
+  width: 100%;
+  height: 100%;
+  transition: 0.1s ease-in;
+
+  &:hover {
+    filter: invert(70%) sepia(0%) saturate(1441%) hue-rotate(193deg) brightness(88%) contrast(93%);
   }
 `
 
@@ -67,33 +93,32 @@ const Submit = styled.button`
   cursor: pointer;
 `
 
+
+
 function ModalAddProduct() {
 
   function handleOnClick(e) {
-    e.preventDefault()
-
-    const rect = e.target.getBoundingClientRect();
-    const isClickIntoDialog = (rect.top <= e.clientY && e.clientY <= rect.top + rect.height &&
-      rect.left <= e.clientX && e.clientX <= rect.left + rect.width);
-
-    if (!isClickIntoDialog) {
-      window.modalAddProduct.close()
-    }
+    window.modalAddProduct.close()
   }
 
   function handleOnSubmit(e) {
     e.preventDefault()
   }
 
+
   return (
-    <Modal id='modalAddProduct' onMouseUp={handleOnClick} onSubmit={handleOnSubmit}>
+    <Modal id='modalAddProduct' onSubmit={handleOnSubmit}>
+
+      <ButtonClose title="Закрыть" onClick={handleOnClick}>
+        <IconClose src={close} />
+      </ButtonClose>
+
       <CommonBox>
         <Title>Добавление нового товара</Title>
 
         <Form>
           <Label htmlFor="linkToProduct">Ссылка на товар</Label>
           <Input type="url" id="linkToProduct" />
-
         </Form>
         <Submit>Отправить</Submit>
       </CommonBox>

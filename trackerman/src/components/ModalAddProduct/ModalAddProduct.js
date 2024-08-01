9cb08327c87da12d6f7e9e78170a01ff1dbd71ef
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-08-01 15:30:40 +0300

    Добавляет форму в модальное окно добавления нового товара

--- trackerman/src/components/ModalAddProduct/ModalAddProduct.js
+++ trackerman/src/components/ModalAddProduct/ModalAddProduct.js
@@ -3,9 +3,9 @@ import styled from "styled-components"
 const Modal = styled.dialog`
   max-width: 650px;
   width: 100%;
-  height: 300px;
   z-index: 99999;
   border: none;
+  padding: 25px;
 
   &::backdrop {
     background: rgba(0, 0, 0, 0.3);
@@ -17,9 +17,61 @@ const Modal = styled.dialog`
   }
 `
 
+const CommonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`
+
+const Title = styled.h3`
+  margin: 0 0 50px 0;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+const Label = styled.label`
+  margin-bottom: 5px;
+`
+
+const Input = styled.input`
+  height: 25px;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:nth-child(1n) {
+    margin-bottom: 15px;
+  }
+
+  &:last-of-type {
+    margin-bottom: 45px;
+  }
+`
+
+const Submit = styled.button`
+  width: 150px;
+  height: 40px;
+  font-size: 15px;
+  align-self: center;
+  cursor: pointer;
+`
+
 function ModalAddProduct() {
 
   function handleOnClick(e) {
+    e.preventDefault()
+
     const rect = e.target.getBoundingClientRect();
     const isClickIntoDialog = (rect.top <= e.clientY && e.clientY <= rect.top + rect.height &&
       rect.left <= e.clientX && e.clientX <= rect.left + rect.width);
@@ -29,8 +81,23 @@ function ModalAddProduct() {
     }
   }
 
+  function handleOnSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <Modal id='modalAddProduct' onClick={handleOnClick}></Modal>
+    <Modal id='modalAddProduct' onMouseUp={handleOnClick} onSubmit={handleOnSubmit}>
+      <CommonBox>
+        <Title>Добавление нового товара</Title>
+
+        <Form>
+          <Label htmlFor="linkToProduct">Ссылка на товар</Label>
+          <Input type="url" id="linkToProduct" />
+
+        </Form>
+        <Submit>Отправить</Submit>
+      </CommonBox>
+    </Modal>
   )
 }
 

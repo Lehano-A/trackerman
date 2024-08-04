2ebbb6635a0b88246cad4c92e7f1e8b3ef823c89
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-08-04 15:52:12 +0300

    Добавляет библиотеку @redux-devtools/extension

--- trackerman/src/redux/store/store.js
+++ trackerman/src/redux/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { modalDeleteCardSlice } from '../slices/ModalDeleteCardSlice'
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    modalDeleteCard: modalDeleteCardSlice.reducer,
+  },
 })

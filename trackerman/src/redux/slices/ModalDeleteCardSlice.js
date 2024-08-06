fae0bd35309a1d2dfaaebb7459b1892899a03e24
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-08-06 14:56:18 +0300

    Создаёт слайс для ModalDeleteCard

---
+++ trackerman/src/redux/slices/ModalDeleteCardSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  isVisible: null,
+  dataDeletingCard: null,
+}
+
+export const modalDeleteCardSlice = createSlice({
+  name: 'modalDeleteCard',
+  initialState,
+  reducers: {
+
+    showModal: (state) => {
+      state.isVisible = true
+    },
+
+    hideModal: (state) => {
+      state.isVisible = false
+    },
+
+    saveDataDeletingCard: (state, actions) => {
+      state.dataDeletingCard = actions.payload
+    },
+  },
+})
+
+
+export const { showModal, hideModal, saveDataDeletingCard } = modalDeleteCardSlice.actions
+
+export default modalDeleteCardSlice.reducer

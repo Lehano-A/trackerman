cc179f732a21e0185835720a956d1e83f1ad8fdd
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-08-23 16:06:39 +0300

    Добавляет логику обработчикам промиса при запросе стартового списка товаров

--- src/api/getProductList/getProductList.js
+++ src/api/getProductList/getProductList.js
@@ -1,9 +1,18 @@
+import { disableLoadingIndicator } from "../../redux/slices/mainBlockSlice"
+import { setOfflineConnectionStatus, setOnlineConnectionStatus } from "../../redux/slices/serverSlice"
 import api from "../api"
 
 function getProductList(dispatch, action) {
   api.getProductList()
-    .then((products) => dispatch(action(products)))
-    .catch((err) => console.log('Ошибка: ', err))
+    .then((products) => {
+      dispatch(action(products))
+      dispatch(setOnlineConnectionStatus())
+    })
+    .catch((err) => {
+      dispatch(setOfflineConnectionStatus())
+      console.log('Ошибка: ', err)
+    })
+    .finally(() => dispatch(disableLoadingIndicator()))
 }
 
 export { getProductList }
